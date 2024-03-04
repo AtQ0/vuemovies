32c51df7993019592857a4c0e81974a9bbf94d71
AtQ0 <mail@atkobabic.com>
2024-03-04 19:51:54 +0100

    added some structure to the project

---
+++ app.vue
@@ -0,0 +1,39 @@
+<template>
+  <Header />
+ <!-- <Header></Header> -->
+
+  <main>
+    <h1 @click="nameGenerator()">Hello World</h1>
+  </main>
+
+
+
+</template>
+
+<script setup>
+
+  //Import ref in order to enable creation of variables
+  import { ref } from "vue";
+
+  //Import used components
+  import Header from "@/layouts/Header.vue"
+
+  const name = ref("Alice")
+
+
+  function nameGenerator(){
+
+    console.log(name.value)
+  }
+
+</script>
+
+
+
+<style scoped>
+  h1{
+    cursor: pointer;
+    background-color: lightblue;
+  }
+
+</style>
